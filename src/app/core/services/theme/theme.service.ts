f850aa3280e6430ef01a6f17aa3bf51f38c1f78d
fabrice <fnduister@gmail.com>
2019-07-28 04:35:50 -0400

    theming finished

---
+++ src/app/core/services/theme/theme.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Theme } from "../../models/theme/theme.module";
+
+const ThemesData = [{
+  name: "student",
+  css_name: "student-main-theme"
+},
+{
+  name: "teacher",
+  css_name: "teacher-main-theme"
+}]
+
+@Injectable({
+  providedIn: "root"
+})
+export class ThemeService {
+  private themeProvider = new BehaviorSubject<string>("teacher-main-theme");
+  currentTheme$: Observable<string> = this.themeProvider.asObservable();
+
+  constructor() {}
+
+  changeTheme = (theme_name: string) => {
+    const newTheme: Theme = ThemesData.find(theme => theme.name === theme_name);
+    if(newTheme){
+      this.themeProvider.next(newTheme.css_name);
+    }
+  }
+}
