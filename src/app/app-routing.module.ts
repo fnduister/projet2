eecf0a1d814dcd3ce72747b9b665b889e677d9c4
aicha <miloudi.ai04@gmail.com>
2019-08-10 01:46:45 -0400

    added guard to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,25 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { LoginController } from "./components/login/login.controller";
 import { HomeController } from "./components/home/home.controller";
-import { PageNotFoundController } from './components/page-not-found/page-not-found.controller';
+import { PageNotFoundController } from "./components/page-not-found/page-not-found.controller";
 import { DashboardController } from "./components/dashboard/dashboard.controller";
+import { AuthGuard } from "./auth/auth.guard";
 
 const routes: Routes = [
   {
     path: "",
     component: HomeController,
+    canActivate: [AuthGuard],
     children: [{ path: "", component: DashboardController }]
   },
   { path: "login", component: LoginController },
-  { path: "not-found", component: PageNotFoundController},
-  { path: "**", redirectTo: "not-found"}
+  { path: "not-found", component: PageNotFoundController },
+  { path: "**", redirectTo: "not-found" }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-exports: [RouterModule]
+
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
