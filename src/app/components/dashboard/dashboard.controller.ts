40dd731e8e600ae60af731cf09678aca3b9d8a50
Fabrice Ndui <fnduister@gmail.com>
2019-08-12 20:08:21 -0400

    starting dashboard

--- src/app/components/dashboard/dashboard.controller.ts
+++ src/app/components/dashboard/dashboard.controller.ts
@@ -1,12 +1,24 @@
 import { Component } from "@angular/core";
-import { ɵHttpInterceptingHandler } from '@angular/common/http';
+import { ɵHttpInterceptingHandler } from "@angular/common/http";
 
+interface Course {
+  title: string;
+  tps: number;
+  tds: number;
+  friends: number;
+  priority: number;
+}
 @Component({
   selector: "home-dashboard",
   templateUrl: "./dashboard.view.html",
   styleUrls: ["./dashboard.scss"]
 })
 export class DashboardController {
-    text: string = "in dashboard";
+  text: string = "in dashboard";
+  courses: Course[] = [
+    { title: "LOG2420", tps: 5, tds: 3,friends: 2, priority: 1 },
+    { title: "INF4565", tps: 2, tds: 4,friends: 4, priority: 2 },
+    { title: "TEC9823", tps: 6, tds: 3,friends: 6, priority: 3 },
+    { title: "BIO0234", tps: 1, tds: 2,friends: 5, priority: 1 }
+  ];
 }
-
