4f56e704d813934c762fbc6f295d023ab32048de
aicha <miloudi.ai04@gmail.com>
2019-08-17 21:09:12 -0400

    added router to login to redirect to home page when logged in

--- src/app/components/login/login.controller.ts
+++ src/app/components/login/login.controller.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { AuthService } from "src/app/auth/auth.service";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-login",
@@ -15,11 +16,12 @@ export class LoginController {
     this.login(form.value);
   }
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService,private router:Router) {}
 
   login({ email, password, university }) {
     this.auth.emailSignin({ email, password, university });
     console.log("loggin in");
+    return this.router.navigate([" "]);
   }
   logout() {
     this.auth.signOut();
